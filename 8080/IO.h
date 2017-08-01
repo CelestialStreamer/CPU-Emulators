1fe77f0d617d807c045241cdc7e972b6c43ad1a3
Brian Woodruff <m.brian.woodruff@gmail.com>
2017-08-01 14:59:13 -0600

    It's been a while. Refactered a lot of code. Should still run fine.

---
+++ 8080/IO.h
@@ -0,0 +1,84 @@
+#pragma once
+#include <cstdint>
+
+class IO
+{
+public:
+   uint8_t read(uint8_t port);
+   void write(uint8_t port, uint8_t value);
+
+private:
+   struct Read1
+   {
+      uint8_t coin : 1; // Coin (0 when active)   
+      uint8_t player2Start : 1;
+      uint8_t player1Start : 1;
+      uint8_t fill1 : 1; // ?
+      uint8_t player1Shoot : 1;
+      uint8_t player1joystickLeft : 1;
+      uint8_t player1joystickRight : 1;
+      uint8_t fill2 : 1; // ?
+   } Read1;
+   struct Read2
+   {
+      uint8_t lives : 2; // Dipswitch number of lives (0:3,1:4,2:5,3:6)
+      uint8_t tilt : 1; // Tilt 'button'
+      uint8_t bonusLife : 1; // Dipswitch bonus life at 1:1000,0:1500    
+      uint8_t player2Shoot : 1;
+      uint8_t player2joystickLeft : 1;
+      uint8_t player2joystickRight : 1;
+      uint8_t coinInfo : 1; // Dipswitch coin info 1:off,0:on  
+   } Read2;
+
+   uint8_t shift_offset;
+
+   uint8_t shift0;
+   uint8_t shift1;
+};
+
+uint8_t IO::read(uint8_t port)
+{
+   switch (port)
+   {
+   case 1:
+   {
+      return *((uint8_t*)&Read1);
+   }
+   case 2:
+   {
+      return *((uint8_t*)&Read2);
+   }
+   case 3:
+   {
+      // Shift register result
+      return ((((shift1 << 8) | shift0) >> (8 - shift_offset)) & 0xff);
+   }
+   default:
+      return 0;
+   }
+}
+
+void IO::write(uint8_t port, uint8_t value)
+{
+   switch (port)
+   {
+   case 2:
+   {
+      // Shift register result offset (bits 0,1,2)
+      shift_offset = value & 0x7;
+      break;
+   }
+   case 3: break; // Sound related, unemulated
+   case 4:
+   {
+      // Fill shift register
+      shift0 = shift1;
+      shift1 = value;
+      break;
+   }
+   case 5: break; // Sound related, unemulated
+   case 6: break; // strange 'debug' port? eg. it writes to this port when it writes text to the screen(0 = a, 1 = b, 2 = c, etc)
+   default:
+      break;
+   }
+}
--- 8080/Source.cpp
+++ 8080/Source.cpp
@@ -1,7 +1,9 @@
 #include "State8080.h"
-
+#include "IO.h"
 #include <iostream>
 #include <fstream>
+
+
 
 int main(int argc, char** argv)
 {
@@ -9,40 +11,20 @@ int main(int argc, char** argv)
       return 0;
 
    State8080* state = new State8080;
-   State8080::RReg* Reg = &state->Reg;
 
    std::ifstream file(argv[1], std::ios::binary);
-
    file.read((char*)state->memory, 0xffff);
+   file.close();
 
-   //int pc = 0;
-   //while (pc < 1453)
-   //{
-   //   pc = pc + Disassemble8080Op(state->memory, pc);
-   //   std::cout << std::endl;
-   //}
+   IO io;
 
-   //state->memory[0x00] = 0xc3;
-   //state->memory[0x01] = 0x00;
-   //state->memory[0x02] = 0x01;
-
-   //state->memory[0x170] = 0x07;
-
-   //uint8_t program[] =
-   //{
-   //   0x3e, 0x01, // MVI A, $1
-   //   0x06, 0x02, // MVI Reg.b, $2
-   //   0x80        // ADD Reg.b
-   //};
-
-   //for (int opcode = 0; opcode < sizeof(program); opcode++)
-   //   state->memory[opcode] = program[opcode];
-
-   // Made it to 0x0588
+   state->in = io.read;
+   state->out = io.write;
 
    for (;;)
    {
       std::cout << std::endl;
+      state->Disassemble8080Op();
       state->Emulate8080Op();
       state->display();
    }
