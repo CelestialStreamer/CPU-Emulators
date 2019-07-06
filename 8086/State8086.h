84306f909ddba777e2530f182d9671213f8d9d4f
Brian Woodruff <m.brian.woodruff@gmail.com>
2019-07-06 04:57:09 -0600

    Add tweaks for better readability.
    Put mod/reg/rm variables inside modregrm struct.
    Put flags inside ALU.
    Cleanup prefix syntax.
    Use binary literals instead of hex literals for opcode use.
    Add imm() to State8086 which accesses memory and updates IP appropriately.
    Refactor variables inside many function calls (comments added to describe their purpose bettern than a variable name would)

--- 8086/State8086.h
+++ 8086/State8086.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "alu.h"
 #include "IO.h"
 #include "Memory.h"
 
@@ -75,49 +76,6 @@ public:
    // This value is can be saved on the stack and later restored
    uint16_t IP; // Instruction Pointer
 
-   // Status Flags:
-   // O, S, Z, A, P, C
-   // Controll Flags:
-   // I, D, T
-   struct Flags {
-      // F E D C B A 9 8 7 6 5 4 3 2 1 0
-      // - - - - O D I T S Z - A - P - C
-
-      // An arithmetic overflow has occurred.
-      // An Interrupt On Overflow instruction is available (which?)
-      uint8_t O = 0; // Overflow
-      // When set, string instructions auto-decrement;
-      // that is, to processs strings from high addresses to low addresses,
-      // or from "right to left". Clearing causes "left to right"
-      uint8_t D = 0; // Direction
-      // Setting allows CPU to recognize external (maskable) interrupt requests.
-      // This flag has no affect on either non-maskable external or internally generated interrupts.
-      uint8_t I = 0; // Interrupt
-      // Setting this puts the CPU into single-step mode for debugging.
-      // In this mode, the CPU automatically generates an internal interrupt after each instruction.
-      uint8_t T = 0; // Trap
-      // If set, the high-order bit of the result is 1.
-      // 0 => positive, 1 => negative
-      uint8_t S = 0; // Sign
-      // Result of operation is zero
-      uint8_t Z = 0; // Zero
-      // There has been a
-      //    carry out of the low nibble into the high nibble
-      // or a
-      //    borrow from the high nibble into the low nibble
-      // of an 8-bit quantity (low-order byte of a 16-bit quantity)
-      uint8_t A = 0; // Auxiliary Carry
-      // If set, the result has even parity, an even number of 1-bits
-      uint8_t P = 0; // Parity
-      // There has been a
-      //    carry out of, or a borrow into,
-      // the high-order bit of the result (8- or 16-bit)
-      uint8_t C = 0; // Carry
-
-      template<typename T> void set(T);
-      template<typename T> T get();
-
-   } flags;
 
    void reset() {
       IP = 0;
@@ -125,48 +83,60 @@ public:
    }
 
    void externalInterrupt(uint8_t vector) {
-      if (flags.I)
+      if (alu.flags.I)
          interrupt(vector);
    }
 
-   void Emulate8086Op(int runtime);
+   void run(int runtime);
 
-   //private:
+private:
    IO * io;
+   ALU alu;
    Memory * mem;
 
-   uint8_t mode, reg, rm;
+   struct ModRegRm {
+      int mode, reg, rm;
+      void operator()();
+   } modregrm;
+
    uint16_t disp16;
    uint16_t segment;
    bool segoverride;
 
    bool halted;
-   template<typename T> T    readrm(uint8_t rmval);
-   template<typename T> void writerm(uint8_t rmval, T value);
+
+   // register/memory access
+   template<typename T> T    readrm();
+   template<typename T> void writerm(T value);
 
    // register access
    template<typename T> T& fetchreg(uint8_t regid);
 
    // other
-   uint16_t incrementIP(uint16_t inc) { return IP += inc; }
-   void modregrm();
+   template<typename T>
+   T imm() {
+      T value = mem->getmem<T>(segRegs.CS, IP);
+      IP += sizeof(T);
+      return value;
+   }
+
    void push(uint16_t value);
    uint16_t pop();
    void interrupt(uint8_t vector);
-   int getea(uint8_t rmval);
+   int getea();
 };
 
 
 
 template<typename T>
-T State8086::readrm(uint8_t rmval) {
-   if (mode == 3) return fetchreg<T>(rmval);
-   else           return mem->getmem<T>(getea(rmval));
+T State8086::readrm() {
+   if (modregrm.mode == 3) return fetchreg<T>(modregrm.rm);
+   else                    return mem->getmem<T>(getea());
 }
 
 template<typename T>
-void State8086::writerm(uint8_t rmval, T value) {
-   if (mode == 3) fetchreg<T>(rmval) = value;
-   else           mem->putmem(getea(rmval), value);
+void State8086::writerm(T value) {
+   if (modregrm.mode == 3) fetchreg<T>(modregrm.rm) = value;
+   else                    mem->putmem(getea(), value);
 }
 
